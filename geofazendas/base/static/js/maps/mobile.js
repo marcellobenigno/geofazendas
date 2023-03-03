6be742a0298f7d9e0b38782e8318eda0afa601e6
Marcello Benigno <benigno.marcello@gmail.com>
2023-03-03 17:59:11 -0300

    ajusta mobile

--- geofazendas/base/static/js/maps/mobile.js
+++ geofazendas/base/static/js/maps/mobile.js
@@ -1,4 +1,20 @@
 var bounds = JSON.parse($('#extent').val());
+var id_municipio = $('#id_municipio').val();
+var tematico = $('#tematico').val();
+var municipio_nome = $('#municipio_nome').val();
+
+console.log(themeList[tematico])
+
+var municipio_selecionado = L.tileLayer.wms(geoServerUrl, {
+    format: 'image/png',
+    transparent: true,
+    version: '1.1.0',
+    maxZoom: 20,
+    opacity: 1,
+    zIndex: 5,
+    layers: 'geofazendas:mapas_municipio_selecionado',
+    cql_filter: `municipio_id=${id_municipio}`
+});
 
 const map = L.map('map', {
     maxZoom: 22,
@@ -21,6 +37,21 @@ const baseLayers = {
 
 const overlays = {
     'Imóveis Rurais': car,
+    'Municípios': municipio,
 };
 
-L.control.layers(baseLayers, overlays).addTo(map);
+var layerControl = L.control.layers(baseLayers, overlays).addTo(map);
+
+layerControl.addOverlay(municipio_selecionado, municipio_nome);
+
+
+municipio.addTo(map);
+municipio_selecionado.addTo(map);
+
+if (tematico) {
+    layerControl.addOverlay(themeList[tematico].geolyr, themeList[tematico].nome);
+    municipio.bringToFront()
+    municipio_selecionado.bringToFront()
+
+}
+
